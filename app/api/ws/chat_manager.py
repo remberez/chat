f10a09b396bba2d7b1af7a80440747a149c5be4e
artem <agasyan222@gmail.com>
2025-03-27 16:28:22 +0400

    rework manager

--- app/api/ws/chat_manager.py
+++ app/api/ws/chat_manager.py
@@ -1,16 +1,27 @@
 from fastapi.websockets import WebSocket
+from dataclasses import dataclass
+
+
+@dataclass
+class Client:
+    websocket: WebSocket
+    name: str | None = None
+    room: int | None = None
 
 
 class WebSocketChatManager:
-    def __init__(self, websocket: WebSocket):
-        self.websocket = websocket
-        self.name = None
-        self.room = None
+    def __init__(self):
+        self.clients: list[Client] = []
 
-    async def accept(self) -> None:
-        await self.websocket.accept()
+    async def accept(self, websocket: WebSocket) -> None:
+        await websocket.accept()
+        data = await websocket.receive_json()
+        name, room = data.get('name', None), data.get('room', None)
 
-    async def initialize_data(self) -> None:
-        data = await self.websocket.receive_json()
-        self.name = data.get("name")
-        self.room = data.get("room")
+        if name and room:
+            self.clients.append(Client(websocket=websocket, name=name, room=room))
+        else:
+            await websocket.close()
+
+
+ws_manager = WebSocketChatManager()
