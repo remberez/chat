f7d432caf5195c5a19e95dac53be23e7ba24fb88
artem <agasyan222@gmail.com>
2025-03-27 19:58:32 +0400

    add react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ChatRoom from './components/ChatRoom';
+import ConnectForm from './components/ConnectForm';
+
 function App() {
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/chat/:roomId" element={<ChatRoom />} />
+        <Route path="/" element={<ConnectForm />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
